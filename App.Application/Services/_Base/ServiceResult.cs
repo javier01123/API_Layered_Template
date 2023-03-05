c8715ba32a6b0e6be9aac414b4bdb33ee0694d4e
Javier Andrade <javier01123@gmail.com>
2023-03-04 17:54:02 -0700

    class for services result

---
+++ App.Application/Services/_Base/ServiceResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Application.Services._Base
+{
+    public class ServiceResult<T>
+    {
+        private ServiceResult(List<Error> errors, T returnValue)
+        {
+            Errors = errors.AsReadOnly();
+            ReturnValue = returnValue;
+        }
+
+        public ReadOnlyCollection<Error> Errors { get; private set; }
+        public T ReturnValue { get; private set; }
+        public bool IsSuccesful => !Errors.Any();
+
+
+        public static ServiceResult<T> ForFailure(List<Error> errors)
+        {
+            if(errors == null) throw new ArgumentNullException(nameof(errors));  
+            if(!errors.Any()) throw new ArgumentNullException($"{nameof(errors)} cannot be empty");
+            return new ServiceResult<T>(errors, default);
+        }
+
+        public static ServiceResult<T> ForSuccess(T returnValue)
+        {
+            return new ServiceResult<T>(new List<Error>(), returnValue);
+        }
+    }
+}
