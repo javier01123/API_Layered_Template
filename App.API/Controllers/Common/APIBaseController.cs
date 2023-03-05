fc72a190cbd1f1ba6fe94ba10319058639dcf446
Javier Andrade <javier01123@gmail.com>
2023-03-04 17:22:58 -0700

    folders and packages

---
+++ App.API/Controllers/Common/APIBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.API.Controllers.Common
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    internal abstract class APIBaseController : ControllerBase
+    {
+    }
+}
---
+++ App.API/Middleware/AppExceptionsMiddleware.cs
@@ -0,0 +1,15 @@
+﻿namespace App.API.Middleware
+{
+    public class AppExceptionsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AppExceptionsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+          
+
+    }
+}
